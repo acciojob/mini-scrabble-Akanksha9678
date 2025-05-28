7f4e553dd500f4de21ea09f47d5deac910894330
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-29 03:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let count=0;
+let input=document.querySelector("#evaluatedText");
+input.addEventListener("input",()=>{
+	count=input.value.length;
+	let h=document.querySelector("#letterCount");
+	h.innerText=count;
+})
